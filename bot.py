d66040f7bd739fc05c0204eb2c4d4061b6c81342
Aratrika Pal <dr.biplabpal@gmail.com>
2021-08-01 00:42:40 +0800

    first command set

--- bot.py
+++ bot.py
@@ -1,4 +1,15 @@
 import telebot
 import os
+from dotenv import load_dotenv
 
-API_KEY=os.getenv('API_KEY')
+load_dotenv()
+API_KEY=os.getenv("API_KEY")
+print(API_KEY)
+
+bot=telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['start','greet'])
+def greet(message):
+    bot.reply_to(message,"Hello! Welcome to your personal Trader Buddy")
+
+bot.polling()
